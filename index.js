fc9e6e496e5c450936617244ff198deed21bf23c
Alex Ewerlöf <hanifbbz@gmail.com>
2019-05-02 16:52:42 +0200

    make it more error proof for when main is not async

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const [,, ...params] = process.argv
 function am(asyncMain) {
-    asyncMain(...params).catch(e => {
+    try {
+        const mainResult = asyncMain(...params)
+        if (typeof mainResult.catch === 'function') {
+            mainResult.catch(e => {
+                process.exitCode = 1
+                console.error(e)
+            })
+        }
+    } catch (e) {
         process.exitCode = 1
         console.error(e)
-    })
+    }
 }
 
 module.exports = am
