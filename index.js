cb9a8311af7a86f4be4289fb9eca5bc8df55a1c8
Alex Ewerlöf <userpixel@users.noreply.github.com>
2019-05-02 17:28:48 +0200

    fixed a typo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const [,, ...params] = process.argv
 
 function errorHandler(error) {
     process.exitCode = process.exitCode || 1
-    console.error(e)
+    console.error(error)
 }
 
 function am(asyncMain) {
