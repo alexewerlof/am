acc54a7040bb84fddeb9e27bd05e7aedeb7c09a4
Alex Ewerlöf <hanifbbz@gmail.com>
2019-05-06 10:36:30 +0200

    make it not break in the browser environment

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
-const [,, ...params] = process.argv
-
 function errorHandler(error) {
     process.exitCode = process.exitCode || 1
     console.error(error)
 }
 
-process.on('unhandledRejection', (error, failedPromise) => {
-    console.warn('Unhandled Rejection at: Promise', failedPromise)
-    errorHandler(error)
-})
+function listenToUnhandledRejection(process) {
+    if (typeof process === 'object' && !listenToUnhandledRejection.enabled) {
+        listenToUnhandledRejection.enabled = true
+        process.on('unhandledRejection', (error, failedPromise) => {
+            console.warn('Unhandled Rejection at: Promise', failedPromise)
+            errorHandler(error)
+        })
+    }
+}
+
+function getScriptArgs(process) {
+    if (typeof process === 'object' && Array.isArray(process.argv)) {
+        const [,, ...params] = process.argv
+        return params 
+    }
+    return []
+}
 
 function am(asyncMain) {
     try {
-        const mainResult = asyncMain(...params)
+        listenToUnhandledRejection(process)
+        const mainResult = asyncMain(...getScriptArgs(process))
         if (typeof mainResult.catch === 'function') {
             mainResult.catch(errorHandler)
         }
