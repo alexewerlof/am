478584bb5b4b760e534069b7316cb62b959a2307
Alex Ewerlöf <hanifbbz@gmail.com>
2019-12-11 23:28:39 +0100

    implement the initial ideas

--- index.js
+++ index.js
@@ -1,39 +1,74 @@
-function defaultErrorHandler(error) {
-    if (typeof process === 'object') {
-        process.exitCode = process.exitCode || 1
-    }
-    console.error(error)
+function isObj(x) {
+    return x && typeof x === 'object'
 }
 
-function listenToUnhandledRejection(process, errorHandler) {
-    if (typeof process === 'object' && !listenToUnhandledRejection.enabled) {
-        listenToUnhandledRejection.enabled = true
-        process.on('unhandledRejection', (error, failedPromise) => {
-            console.warn('Unhandled Rejection at: Promise', failedPromise)
+function isFn(x) {
+    return typeof x === 'function'
+}
+
+function getGlobal() {
+    if (isObj(process)) {
+        return process
+    }
+    if (isObj(window)) {
+        return window
+    }
+    if (isObj(Worker)) {
+        return Worker
+    }
+    throw new Error('Could not get global')
+}
+
+function defaultErrorHandler(error) {
+    if (isObj(process)) {
+        process.exitCode = process.exitCode || 1
+    }
+    if (isObj(console)) {
+        console.error(error)
+    }
+}
+
+const addedErrorHandlers = new WeakSet()
+
+function listenToUnhandledRejection(errorHandler) {
+    if (!addedErrorHandlers.has(errorHandler)) {
+        addedErrorHandlers.add(errorHandler)
+        getGlobal().on('unhandledRejection', (error, failedPromise) => {
+            if (isObj(console) && isFn(console.warn)) {
+                console.warn('Unhandled Rejection at: Promise', failedPromise)
+            }
             errorHandler(error)
         })
     }
 }
 
 function getScriptArgs(process) {
-    if (typeof process === 'object' && Array.isArray(process.argv)) {
+    if (isObj(process) && Array.isArray(process.argv)) {
         const [, , ...params] = process.argv
         return params
     }
     return []
 }
 
-function am(asyncMain, errorHandler = defaultErrorHandler) {
+function am(asyncMain, options) {
+    const errorHandler = isObj(options) && isFn(options.errorHandler) ?
+        options.errorHandler : defaultErrorHandler
+    const unhandledRejectionHandler = isObj(options) && isFn(options.unhandledRejectionHandler) ?
+        options.unhandledRejectionHandler : errorHandler
+
     try {
-        listenToUnhandledRejection(process, errorHandler)
+        listenToUnhandledRejection(unhandledRejectionHandler)
         const mainResult = asyncMain(...getScriptArgs(process))
-        if (mainResult !== undefined && typeof mainResult.catch === 'function') {
+        // If asyncMain is indeed an async function, it'll always return a promise and promises have a .catch() method
+        if (mainResult !== undefined && isFn(mainResult.catch)) {
             mainResult.catch(errorHandler)
         }
     } catch (error) {
+        // If asyncMain is indeed a sync function, its possible extensions will be handled here
         errorHandler(error)
     }
 }
 
 module.exports = am
 module.exports.am = am
+module.exports.ifMain = ifMain
